5ec8b3486bb12ae67f83e2618fed0f3fc1470bc7
bmayhook9-bit <bmayhook9@gmail.com>
2025-09-16 13:34:31 +0100

    Update User.d.ts

--- server/models/User.d.ts
+++ server/models/User.d.ts
@@ -1,15 +1,6 @@
 declare const _default: {
-  createUser(
-    username: string,
-    password: string,
-    tier?: string,
-  ): Promise<{ id: number; username: string; tier: string }>;
-  findByUsername(
-    username: string,
-  ): Promise<
-    | { id: number; username: string; password: string; tier?: string }
-    | undefined
-  >;
+  createUser(username: string, password: string, tier?: string): Promise<{ id: number; username: string; tier: string }>;
+  findByUsername(username: string): Promise<{ id: number; username: string; password: string; tier?: string } | undefined>;
   close(): Promise<void>;
 };
 export default _default;
